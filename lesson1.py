32cc5e8caa0feda112378aec450896dec457f7c8
henor0 <148788969+henor0@users.noreply.github.com>
2024-10-26 12:46:25 +0200

    ...

---
+++ lesson1.py
@@ -0,0 +1,84 @@
+#Declaring a variable
+from unittest import removeResult
+
+temperature = 22.0
+
+name = "Henor"
+
+print(type(temperature))
+print(type(name))
+
+age = 17
+
+x = 5
+y = 8
+
+resul = x+y
+
+print(result)
+#update value
+age = age+1
+print (age)
+
+#combine values
+
+first_name = "Henor"
+last_name = "Gashi"
+
+full_name = first_name +" " + last_name
+print(full_name)
+
+#Linsts
+
+name_of_the_list = ["item1", "item2", "item3"]
+
+#exemple of a list
+
+shopping_list = ["apple," "bananas", "milk" 3 , 4.5]
+
+print(shopping_list)
+
+#indeksimi
+
+homework = ["math", "psikologji", "biologji", "anglisht"]
+print(homework[0])
+print(homework[3])
+print(homework[1])
+
+to_do_list = {"teach 11D","Go shopping", "creating the app", "check devops"}
+firs_item = to_do_list[0] #this is accsessing the first item at index 0, wich is "teach 11D"
+second_item = to_do_list[1]
+third_item = to_do_list[2]
+
+favorite_subject = ["psikologji", "sociologji", "ed.fizike", "frangjisht"]
+forth_subject = favorite_subject[3]
+print(forth_subject)
+
+#Adding Data to a list we do so by using the append() method
+
+#I want to add strawberries to the shopping list
+shopping_list.append("strawberries")
+
+print(shopping_list)
+
+#insert method lets us specify where we want to add or element
+
+shopping_list.insert(_index:2, _object:"lemons")
+print(shopping_list)
+
+#remove()methods help  us remove items from the list
+
+to_do_list.remove("check devops")
+print(to_do_list)
+print(to_do_list)
+
+#updeting data, uou can change the value of an item by assigning a new value to an index
+to_do_list[0] = "teach 1D"
+
+print(to_do_list)
+
+
+
+
+
+
